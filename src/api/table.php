e5be364f44ed6f5c352ac1091856b0459b9a0326
efri-yang <948061564@qq.com>
2018-03-19 13:47:04 +0800

    c

--- src/api/table.php
+++ src/api/table.php
@@ -1,9 +1,23 @@
 <?php
-header("Access-Control-Allow-Origin: http://a.com"); // 允许a.com发起的跨域请求  
 //如果需要设置允许所有域名发起的跨域请求，可以使用通配符 *  
 header("Access-Control-Allow-Origin: *"); // 允许任意域名发起的跨域请求  
 header('Access-Control-Allow-Headers: X-Requested-With,X_Requested_With'); 
-$data=$_REQUEST[];
-echo json_encode($data);
-	
+// $con = mysql_connect("localhost","haodianbangdan","jqgrid");
+
+$con = mysql_connect("bdm300375458.my3w.com","bdm300375458","mysql3862749");
+if (!$con){
+  die('Could not connect: ' . mysql_error());
+}
+mysql_query("set names utf8");
+mysql_select_db("bdm300375458_db",$con);
+
+$sql="select * from jqgrid";
+$result=mysql_query($sql,$con);
+$arr=[];
+while ($row = mysql_fetch_assoc($result)) {
+	$arr[]=$row;
+}
+
+  $data=$_REQUEST;
+  echo json_encode($data);
 ?>
