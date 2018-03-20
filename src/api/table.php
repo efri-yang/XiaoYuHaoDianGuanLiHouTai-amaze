a9c0786190bba11bf6e6efe7d0787ea64a06c0d2
efri-yang <948061564@qq.com>
2018-03-20 08:55:14 +0800

    c

--- src/api/table.php
+++ src/api/table.php
@@ -1,62 +1,44 @@
 <?php
-//如果需要设置允许所有域名发起的跨域请求，可以使用通配符 *  
-header('Access-Control-Allow-Origin:*');    
-// 响应类型    
-header('Access-Control-Allow-Methods:POST');    
-// 响应头设置    
-header('Access-Control-Allow-Headers:x-requested-with,content-type');  
+//如果需要设置允许所有域名发起的跨域请求，可以使用通配符 *
+header('Access-Control-Allow-Origin:*');
+// 响应类型
+header('Access-Control-Allow-Methods:POST');
+// 响应头设置
+header('Access-Control-Allow-Headers:x-requested-with,content-type');
 
-
-
+//draw length start
 //
-//	_search:false
-//	nd:1521439548263
-//	rows:10
-//	page:1
-//	sidx:id
-//	sord:desc
 
-$rows=$_POST["rows"];
-$page=$_POST["page"];
+$draw = $_POST["draw"];
+$start = $_POST["start"];
+$length = $_POST["length"];
 
-
-$mysqli=new mysqli("bdm300375458.my3w.com","bdm300375458","mysql3862749","bdm300375458_db");
-if($mysqli->connect_errno){
-	printf("Connect failed: %s\n", $mysqli->connect_error);
+$mysqli = new mysqli("bdm300375458.my3w.com", "bdm300375458", "mysql3862749", "bdm300375458_db");
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 }
 $mysqli->query("set names utf8");
-$sql="select count(*) as total from jqgrid";
-$result=$mysqli->query($sql);
+$sql = "select count(*) as total from jqgrid";
+$result = $mysqli->query($sql);
 
+$rowArr = $result->fetch_assoc();
+$recordsTotal = $rowArr["total"]; //返回总页数
+$recordsFiltered = $recordsTotal;
 
-$rowArr= $result->fetch_assoc();
-$records=$rowArr["total"];
+$sql2 = "select  * from jqgrid limit " . $start . "," . $length;
 
-
-
-$total=ceil($records/$rows);
-$start=($page-1)*$rows;
-$end=$page*$rows;
-
-$sql2="select  * from jqgrid limit ".$start.",".$end;
-
-$result=$mysqli->query($sql2);
-$arr=[];
-$arr["page"]=$page;
-$arr["total"]=$total;
-$arr["records"]=$records;
-$k=0;
+$result = $mysqli->query($sql2);
+$arr = [];
+$arr["draw"] = $draw;
+$arr["recordsTotal"] = $recordsTotal;
+$arr["recordsFiltered"] = $recordsFiltered;
+$arr["error"] = 0;
+$k = 0;
 while ($row = $result->fetch_assoc()) {
-	$arr["rows"][$k]["id"]=$row["id"];
-	foreach ($row as $key => $value) {
-		$arr["rows"][$k]["cell"][]=$value;
-	}
-	$k++;
+    $arr["data"][] = $row;
+
 }
-
-
-
 
 echo json_encode($arr);
 
