4a77fc06254b347d27eb70610035f49819b98201
efri-yang <948061564@qq.com>
2018-12-04 17:35:36 +0800

    c

--- src/js/common.js
+++ src/js/common.js
@@ -1,1 +1,18 @@
-
+$(function(){
+	$(".coms-layout-header .bars-nav").on("click",function(event){
+		event.stopPropagation()
+		var $headNav=$(".header-nav");
+		if($headNav.hasClass("active")){
+			$headNav.removeClass("active")
+		}else{
+			$headNav.addClass("active")
+		}
+	});
+	$("body").on("touchstart",function(event){
+		if($(event.target).closest(".header-nav").length){
+			return;
+		}else{
+			$(".header-nav").removeClass("active");
+		}
+	})
+})
